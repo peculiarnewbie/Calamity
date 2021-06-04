e3d57e8aab318da768321250b602cd4aa5a50094
peculiarnewbie <curiousarmand@gmail.com>
2021-06-04 15:20:47 +0700

    Collectible

---
+++ Collectible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    private void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("pick up");
+        Destroy(this.gameObject);
+       
+    }
+}
