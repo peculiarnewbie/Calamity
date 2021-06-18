f459d88b440fb18cff17b4360730d970d404ea59
peculiarnewbie <curiousarmand@gmail.com>
2021-06-18 12:23:20 +0700

    Level Load Triggers

---
+++ TriggerList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum Indicator { Start, Platformer, Combat, Time };
+
+public class TriggerList : MonoBehaviour
+{
+    [SerializeField] private Indicator type;
+    [SerializeField] private int level = 1;
+    [SerializeField] private bool setInactive;
+
+    public event Action OnTimeCollect;
+    public event Action<Indicator> OnLevelTrigger;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (type == Indicator.Time)
+            OnTimeCollect?.Invoke();
+
+        else
+            GameManager.instance.LoadLevel(type);
+
+        if (setInactive)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
