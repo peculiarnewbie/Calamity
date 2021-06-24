a0d5009e86d39f8e5407553a2ebeaa030a81b54c
peculiarnewbie <curiousarmand@gmail.com>
2021-06-24 19:32:46 +0700

    Enemy FSM

--- Scripts/EnemyManager.cs
+++ Scripts/EnemyManager.cs
@@ -9,26 +9,42 @@ public class EnemyManager : MonoBehaviour
 {
     UnityEvent enemyDeathEvent;
     private int enemyHealth;
-    private Animator enemyAnimator;
+    private AnimatorHandler enemyAnimator;
     private IEnumerator enemyDeathCoroutine;
     private bool isDamaged;
 
-    public float lookRadius = 10f;
+    public LayerMask whatIsGround, whatIsPlayer;
 
     Transform target;
     NavMeshAgent agent;
+
+    //walk point
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    //Ranges
+    public float sightRange = 10f;
+    public float attackRange = 5f;
+    public bool targetInSightRange, targetInAttackRange;
 
     private void Start()
     {
         target = PlayerManager.instance.player.transform;
         agent = GetComponent<NavMeshAgent>();
+        enemyAnimator = GetComponent<AnimatorHandler>();
+        enemyAnimator.Initialize();
     }
 
     private void Update()
     {
         float distance = Vector3.Distance(target.position, transform.position);
 
-        if(distance <= lookRadius)
+        if(distance <= sightRange)
         {
             agent.SetDestination(target.position);
 
@@ -38,11 +54,75 @@ public class EnemyManager : MonoBehaviour
                 FaceTarget();
             }
         }
+
+        //Check for sight and attack range
+        targetInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        targetInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (targetInSightRange)
+        {
+            if (targetInAttackRange) 
+                AttackPlayer();
+            else 
+                ChasePlayer();
+        }
+        else 
+            Patroling();
+    }
+
+    private void Patroling()
+    {
+        if (!walkPointSet) 
+            SearchWalkPoint();
+        else 
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(target.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+        FaceTarget();
+
+        if (!alreadyAttacked)
+        {
+            enemyAnimator.PlayAnimationTrigger("Attack");
+            Debug.Log("attacking");
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 
     void FaceTarget()
     {
-        Debug.Log("what");
         Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
@@ -59,7 +139,7 @@ public class EnemyManager : MonoBehaviour
             }
             else
             {
-                enemyAnimator.SetBool("Damaged", true);
+                enemyAnimator.PlayAnimationTrigger("Damaged");
             }
         }
         
@@ -74,7 +154,7 @@ public class EnemyManager : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, lookRadius);
+        Gizmos.DrawWireSphere(transform.position, sightRange);
     }
 
 
